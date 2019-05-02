aa13bbe23f6607a853b75312f19ea18e362b4c9d
Sarah Rodrigues <sarahrodriguesmoreira@gmail.com>
2019-05-02 12:03:24 -0300

    Terminando a prática

--- starwars.js
+++ starwars.js
@@ -1,5 +1,39 @@
-// Seu javascript aqui :)
-// Use a Star Wars API: https://swapi.co/
-// para fazer uma requisição assíncrona e:
-//  - Pegar a lista de filmes (AJAX) e preencher no HTML
-//  - Quando um filme for clicado, exibir sua introdução
+let audio = new Audio("intro.ogg");
+
+if(localStorage.getItem("atual") != null){
+	updateIntro(localStorage.getItem("atual"));
+}
+
+botoes = document.getElementsByClassName("movie_option");
+for(let b of botoes){
+	b.addEventListener("click", function() {
+		audio.currentTime = 0;
+		updateIntro(b.getAttribute("data-value"));		
+		audio.play();
+	});		
+}
+
+
+function updateIntro(number){
+	$.ajax({url: "https://swapi.co/api/films/" + number + "/", success: function(result){		
+		updateElement(result.title, result.episode_id, result.opening_crawl);
+		localStorage.setItem("atual", number);
+	}});
+}
+
+function updateElement(title, id, crawl){
+	let intro = document.getElementsByClassName("reading-animation")[0];
+	intro.innerText = "Episode " + getRom(id) + "\n" + title.toUpperCase() + "\n\n" + crawl;	
+}
+
+function getRom(n){
+	switch(n){
+		case 1: return "I";
+		case 2: return "II";
+		case 3: return "III";
+		case 4: return "IV";
+		case 5: return "V";
+		case 6: return "VI";
+		case 7: return "VII";
+	}
+}
